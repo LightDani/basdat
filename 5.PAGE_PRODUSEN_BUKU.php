805efd8a17c79e5714c19f67da55a5882d0cd07b
beningmoe <65642374+beningmoe@users.noreply.github.com>
2020-05-21 18:23:03 +0700

    Add files via upload

---
+++ 5.PAGE_PRODUSEN_BUKU.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>(BDE) Admin Page</title>
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="container-fluid" style="background: #393939;>
+		<div class="row" align="center">
+			<br><br>
+			<h1 style="font-family: cabin; color: #FFFFFF;">Bright Day Expo</h1>
+			<h2 style="font-family: cabin; color: #FFFFFF;">Selamat Datang di Halaman Pengelola</h2>
+			<br><br>
+				<ul style="color: #FFFFFF">
+					<form>
+  					<div class="form-group">
+   						 <label for="exampleInputEmail1">Email address</label>
+   						 <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+ 					</div>
+ 					<div class="form-group">
+   						 <label for="exampleInputPassword1">Password</label>
+   						 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  					</div>
+  					<div class="form-group">
+    					 <label for="exampleInputFile">File input</label>
+    					 <input type="file" id="exampleInputFile">
+    				<p class="help-block">Example block-level help text here.</p>
+  					</div>
+  					<div class="checkbox">
+    					<label>
+      					<input type="checkbox"> Check me out
+    					</label>
+  					</div>
+ 					 <button type="submit" class="btn btn-warning">Submit</button>
+					</form>
+ 						<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+ 					</div>
+ 				</ul>
+		</div>
+</body>
+</html>
