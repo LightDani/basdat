14d97a802d9d3470d5409579a1abf47a3e6c6859
beningmoe <65642374+beningmoe@users.noreply.github.com>
2020-05-22 18:40:56 +0700

    Add files via upload

---
+++ 3.LIST_KATEGORI_PAGE_CUSTOMER (NON FIKSI).php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>(BDE) Non Fiksi</title>
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="container-fluid" style="background: #393939;>
+		<div class="row" align="left">
+			<br>
+			<div class = "row">
+				<div class="col-md-2" align="center"> 
+					<br>
+  					<img src="3-01.png" width="100" height="100" alt="Responsive image" class="img-rounded">
+  					<br><br><br>
+  				</div>
+ 				<div class="col-md-10">
+					<br>
+					<h1 style="font-family: cabin; color: #FFFFFF;">Bright Day Expo</h1>
+					<h2 style="font-family: cabin; color: #FFFFFF;">List Buku Kategori Non Fiksi</h2>
+					<br><br><br>
+				</div>
+			</div>
+				<ul style="color: #FFFFFF">
+					<div align="left">
+  						<div class="col-md-12">LIST BUKU
+  							<table class="table table-striped">
+  							<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+							</table>
+ 						</div>
+ 					</div>
+ 					<br>
+ 				</ul>
+		</div>			
+	</div>
+</body>
+</html>
