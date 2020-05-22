c4ece26cf14a2e41aabbf42d8eff96b50be05456
beningmoe <65642374+beningmoe@users.noreply.github.com>
2020-05-22 12:02:19 +0700

    Add files via upload

---
+++ 3.LIST_KATEGORI_PAGE_CUSTOMER (NEW).php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>(BDE) List Kategori</title>
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="container-fluid" style="background: #393939;>
+		<div class="row" align="center">
+			<br>
+			<img src="3-01.png" width="150" height="150" alt="Responsive image" class="img-rounded">
+			<br><br>
+			<h1 style="font-family: cabin; color: #FFFFFF;">Bright Day Expo</h1>
+			<h2 style="font-family: cabin; color: #FFFFFF;">Berikut adalah daftar kategori dari koleksi buku yang kami miliki</h2>
+			<br><br>
+				<ul style="color: #FFFFFF">
+					<div class="row">
+						<div class="col-md-6">
+							<input class="btn btn-warning" type="button" value="FIKSI">
+							<br><br><br>
+						</div>
+						<div class="col-md-6">
+							<input class="btn btn-warning" type="button" value="NON FIKSI">
+							<br><br><br>
+						</div>
+						
+ 						<br><br><br><br><br><br><br><br><br><br>
+ 					</div>
+ 				</ul>
+		</div>
+</body>
+</html>
---
+++ 4.PAGE_PRODUSEN (NEW).php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>(BDE) Admin Page</title>
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="container-fluid" style="background: #393939;>
+		<div class="row" align="center">
+			<br><br>
+			<h1 style="font-family: cabin; color: #FFFFFF;">Bright Day Expo</h1>
+			<h2 style="font-family: cabin; color: #FFFFFF;">Selamat Datang di Halaman Pengelola</h2>
+			<br><br>
+				<ul style="color: #FFFFFF">
+					<div class="row">
+						<div class="col-md-4">
+  							<input class="btn btn-warning" type="button" value="List Buku">
+  						    <br>
+  						</div>
+  						<div class="col-md-4">
+  							<input class="btn btn-warning" type="button" value="Tambah Buku">
+  						    <br>
+  						</div>
+  						<div class="col-md-4">
+  							<input class="btn btn-warning" type="button" value="Pemasukan">
+						</div>				
+							</table>
+ 						</div>
+ 						<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+ 					</div>
+ 				</ul>
+		</div>
+</body>
+</html>
